035d6b2e3c28e3d71a809bb351ecf04e079cc024
Tomasz Maćkowiak <kurazu@kurazu.net>
2015-06-11 08:48:03 +0200

    init

---
+++ neural/network.py
@@ -0,0 +1,6 @@
+class NeuralNetwork():
+
+    def __init__(self, number_of_inputs, number_of_outputs, layers_in_between):
+        self.number_of_inputs = number_of_inputs
+        self.number_of_outputs = number_of_outputs
+        self.layers_in_between = layers_in_between
